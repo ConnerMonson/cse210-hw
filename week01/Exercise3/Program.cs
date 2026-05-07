17bfd75651fefc3822eac71e1ee73781b606a797
ConnerMonson <connerrmonson@gmail.com>
2026-05-07 11:57:04 -0500

    finished all exercise assignments

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,32 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1,101);
+
+        int userGuess = -1;
+
+        while (userGuess != number)
+        {
+            Console.WriteLine("Make your Guess: ");
+            userGuess = int.Parse(Console.ReadLine());
+
+            if (userGuess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (userGuess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You Guessed it!");
+            }
+        }
     }
 }
--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,47 @@
-using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+
+        List<int>numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished");
+        int userNumber;
+
+        do
+        {
+            Console.Write("Enter number: ");
+            userNumber = int.Parse(Console.ReadLine());
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        } while (userNumber!= 0);
+
+        int sum = 0;
+        int largest = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+            if (number > largest)
+            {
+                largest = number;
+            }
+        }
+
+        int count = numbers.Count;
+        int average = sum / count;
+
+
+
+
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largest}");
+
     }
 }
