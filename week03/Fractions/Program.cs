ad6e379acf7c3775cde5e63c231153263baa3f4c
ConnerMonson <connerrmonson@gmail.com>
2026-05-18 10:47:25 -0500

    fractions assignment

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -2,8 +2,20 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction f1 = new Fraction();
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.DecimalValue());
+
+        Fraction f2 = new Fraction(5);
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.DecimalValue());
+
+        Fraction f3 = new Fraction(3, 4);
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.DecimalValue());
+   
+   
     }
 }
