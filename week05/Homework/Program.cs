2138dd96252eba8881bdbe06752ee7dc35f967f4
ConnerMonson <connerrmonson@gmail.com>
2026-05-29 11:40:14 -0500

    inheritance assignment

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment = new Assignment("George Bush", "Why dinos die.");
+        Console.WriteLine(assignment.GetSummary());
+
+        MathAssignment mathAssignment = new MathAssignment("Conner", "Math", "4.5", "7-9");
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        WritingAssignment writingAssignment = new WritingAssignment("Conner Monson", "Writing", "The end of the world");
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
